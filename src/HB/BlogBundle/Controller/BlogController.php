47fec4bf761e94cc42775553ea57a6362182368a
Christian Lamargot <clamargot@gmail.com>
2015-04-09 15:53:10 +0200

    install de knp paginator

--- src/HB/BlogBundle/Controller/BlogController.php
+++ src/HB/BlogBundle/Controller/BlogController.php
@@ -6,6 +6,38 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class BlogController extends Controller
-{
+class BlogController extends Controller {
+
+    /**
+     * @Route("/", name="blog_index")
+     * @Route("/page/{page}", name="blog_index_page")
+     * @Template()
+     */
+    public function indexAction($page=1) {
+        // on récupère l'entity manager à l'aide du service Doctrine
+        $em = $this->getDoctrine()->getManager();
+
+        // on récupère le repository de Article et on lui demande  tous les articles
+        //$articles = $em->getRepository('HBBlogBundle:Article')->findAll();
+        $articles = $em->getRepository('HBBlogBundle:Article')->getHomepageArticles($page-1);
+        
+        $nombredepage = $em->getRepository('HBBlogBundle:Article')->getHomepageCountPage();
+        
+        $lienPageSuivante = $page < $nombredepage ?
+                $this->generateUrl("blog_index_page", array("page" => $page+1))
+                : null ;
+
+        $lienPagePrecedente = $page > 1 ?
+                $this->generateUrl("blog_index_page", array("page" => $page-1))
+                : null;
+               
+        // on transmet la liste d'article au template en la nommant entities
+        return array(
+            'articles' => $articles,
+            'lienPageSuivante'=>$lienPageSuivante,
+            'lienPagePrecedente'=>$lienPagePrecedente,
+            'nombredepage'=>$nombredepage,
+        );
+    }
+
 }
